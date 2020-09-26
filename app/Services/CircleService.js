f934df7c60dc3b2c495d7af5e9d9c71476af1eb0
sergioc6 <sergiocabral.1990@gmail.com>
2020-09-26 14:55:16 -0300

    Cambios varios

--- app/Services/CircleService.js
+++ app/Services/CircleService.js
@@ -3,26 +3,51 @@
 class CircleService {
 
     getCirclesWords(arrayWords) {
-        let ret = [];
-
-        for (let i = 0; i < arrayWords.length; i++) {
-            let rest = this.getCirclesWords(arrayWords.slice(0, i).concat(arrayWords.slice(i + 1)));
-
-            if (!rest.length) {
-                ret.push([arrayWords[i]])
+        let permutations = this.getPermutations(arrayWords);
+        
+        permutations.forEach((element, index) => {
+            if (this._checkWords(element[element.length -1], element[0])) {
+                element.push(element[0])
             } else {
-                for (let j = 0; j < rest.length; j++) {
-                    if (this._checkWords(arrayWords[i], rest[j][0])) {
-                        ret.push([arrayWords[i]].concat(rest[j]))
-                    } 
+                permutations.splice(index,1)
+            }
+        });
+        
+        return permutations;
+    }
+
+    getPermutations(list) {
+        // Empty list has one permutation
+        if (list.length == 0)
+            return [[]];
+        var result = [];
+
+        for (var i = 0; i < list.length; i++) {
+            // Clone list (kind of)
+            var copy = Object.create(list);
+
+            // Cut one element from list
+            var head = copy.splice(i, 1);
+
+            // Permute rest of list
+            var rest = this.getPermutations(copy);
+
+            // Add head to each permutation of rest of list
+            for (var j = 0; j < rest.length; j++) {
+                if (this._checkWords(head[head.length -1], rest[j])) {
+                    var next = head.concat(rest[j]);
+                    result.push(next);
                 }
             }
         }
-        return ret;
+        return result;
     }
 
     _checkWords(firstWord, lastWord) {
-        return ((firstWord.substr(firstWord.length - 1)) == (lastWord.substr(0,1)))
+        if (lastWord.length == 0) {
+            return true
+        }
+        return ((firstWord.substr(firstWord.length - 1)) == (lastWord[0].substr(0, 1)))
     }
 
 }
